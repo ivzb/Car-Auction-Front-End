c80a39d0c4a03a7967edb2ca25360a1fe803127e
izahariev <izahariev96@gmail.com>
2017-02-26 09:00:57 +0200

    makes module aligned to standardjs

--- src/app/makes/makes.module.ts
+++ src/app/makes/makes.module.ts
@@ -1,17 +1,17 @@
-import { NgModule }       from '@angular/core';
-import { CommonModule }   from '@angular/common';
-import { FormsModule }    from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { HttpModule, JsonpModule } from '@angular/http'
 
 // components
-import { MakeContainerComponent }  from './make-container.component';
-import { MakeDetailComponent } from './make-detail.component';
+import { MakeContainerComponent } from './make-container.component'
+import { MakeDetailComponent } from './make-detail.component'
 
 // services
-import { MakeService } from './make.service';
+import { MakeService } from './make.service'
 
 // modules
-import { MakesRouting } from './makes.routing';
+import { MakesRouting } from './makes.routing'
 
 @NgModule({
   imports: [
@@ -19,7 +19,7 @@ import { MakesRouting } from './makes.routing';
     FormsModule,
     MakesRouting,
     HttpModule,
-    JsonpModule,
+    JsonpModule
   ],
   declarations: [
     MakeContainerComponent,
